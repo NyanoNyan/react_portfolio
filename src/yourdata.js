2bd7a89d8dff879d35dc9315bbb2b5d7ee7526d0
nyano <nayan100@hotmail.co.uk>
2021-03-10 21:28:48 +0000

    First commit

---
+++ src/yourdata.js
@@ -0,0 +1,201 @@
+// Skills Icons
+import htmlIcon from "./images/html.png"
+import cssIcon from "./images/css.png"
+import reactIcon from "./images/react.png"
+import jsIcon from "./images/javascript.png"
+import designIcon from "./images/design.svg"
+import pythonIcon from "./images/python.png"
+import codeIcon from "./images/code.svg"
+import tensorFlowIcon from "./images/tensorflow.png"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+import twitterIcon from "./images/twitter.png"
+
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Nayan",
+  headerTagline: [
+    //Line 1 For Header
+    "Programmer",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Graduate M.Sc in Computational Cognitive Neuroscience. I love the essence of problem‐solving and I am constantly learning new technologies. A team player who loves to collaborate, lead and motivate to reach project goals.",
+
+  //Contact Email
+  contactEmail: "nayanthapan@yahoo.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Smart Watch Shopping Cart.", //Project Title - Add Your Project Title Here
+      para:
+        "Website for a smartwatch seller.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+         "/imgs/shopping_cart_main.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/NyanoNyan/shopping_cart",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Hotel reviews prediction using Sentiment Analysis", //Project Title - Add Your Project Title Here
+      para:
+        "Classify hotel review inputted by the user.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "/imgs/hotel_review.gif",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/NyanoNyan/Hotel_reviews_sentiment_analysis",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Three", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Four", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      id: 5, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Five", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+    {
+      id: 6, //DO NOT CHANGE THIS (Please)😅
+      title: "Project Six", //Project Title - Add Your Project Title Here
+      para:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "http://chetanverma.com/",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
+    ,{
+        id: 5,
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  aboutParaTwo:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutParaThree:
+    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+
+    {
+      id: 1,
+      img: jsIcon,
+      para:
+        "ES6 Javascript",
+    },
+    {
+      id: 2,
+      img: reactIcon,
+      para:
+        "React",
+    },
+    {
+      id: 3,
+      img: htmlIcon,
+      para:
+        "HTML5",
+    },
+    {
+      id: 4,
+      img: cssIcon,
+      para:
+        "CSS",
+    },
+    {
+      id: 5,
+      img: pythonIcon,
+      para:
+        "Python",
+    },
+    {
+      id: 5,
+      img: tensorFlowIcon,
+      para:
+        "TensorFlow",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Heading",
+  promotionPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+  // contactSubHeading: "Let's create your next experience together",
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/NyanoNyan" },
+    {
+      img: twitterIcon,
+      url: "https://twitter.com/greenNayan",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
